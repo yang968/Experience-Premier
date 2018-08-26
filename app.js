40b630ab0636442c66ec3dbdfc8069503edf74a1
mark <m.arklee@outlook.com>
2018-08-26 14:11:26 -0700

    Add port number 5000

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const express = require('express');
 const db = require("./config/keys").mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000;
