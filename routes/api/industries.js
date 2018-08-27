59cb475ae9107d467336b8d8bf98bcf5f79bc5a3
yang968 <spenceryang89@gmail.com>
2018-08-26 18:35:15 -0700

    Fix error json message for industry

--- routes/api/industries.js
+++ routes/api/industries.js
@@ -7,7 +7,7 @@ const Industry = require('../../models/Industry');
 
 router.post('/', (req, res) => {
   if (req.body.name === undefined || req.body.name === null || req.body.name.length === 0) {
-    return res.status(400).json({"Invalid Industry Name!"});
+    return res.status(400).json({InvalidName: "Invalid Industry Name!"});
   }
 
   const newIndustry = new Industry({
