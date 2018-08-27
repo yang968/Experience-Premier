8cd686876ad48a7cf77d1511d2f3e3fd96a64d5d
Jordan Yu <jordanyu44@gmail.com>
2018-08-26 17:30:31 -0700

    Transfer Gylio stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: __dirname,
-  entry: "./public/javascripts/index.js",
+  entry: "./public/javascripts/index.js", 
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
@@ -12,15 +13,41 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        use: {
+        use: [{
           loader: "babel-loader",
           query: {
             presets: ["env", "react"]
           }
-        }
+        }, {
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader",
+          // options: {
+          //   implementation: require('dart-sass'),
+          //   fiber: Fiber
+          // }
+        }]
+      }, {
+        test: /\.scss$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
-  },
+  }, 
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
   devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", "*"]
