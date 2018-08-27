af88b55fc3cd00e35185086709cdb9ba438b68db
Nick <nmatison@gmail.com>
2018-08-26 20:58:46 -0700

    Add token setup for Watson Speech to Text

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const express = require('express');
+const app = express();
+
 
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
