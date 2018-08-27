d8baf0dfcb14bcac08e6fb156b4f2779577e8268
mark <m.arklee@outlook.com>
2018-08-27 11:55:03 -0700

    Add minor fixes

--- routes/api/users.js
+++ routes/api/users.js
@@ -63,26 +63,20 @@ router.post('/register', (req, res) => {
 
 // Returning User.
 router.post('/login', (req, res) => {
-  const {
-    errors,
-    isValid
-  } = validateLoginInput(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) { return res.status(400).json(errors);}
 
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({ email })
     .then(user => {
-      if (!user) {
-        return res.status(404).json({ email: 'This user does not exist' });
-      }
+      if (!user) { return res.status(404).json({ email: 'This user does not exist' });}
 
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
+            // define payload
             const payload = {
               id: user.id,
               firstName: user.firstName,
@@ -121,7 +115,6 @@ router.post('/login', (req, res) => {
                   });
                 });
              });
-
           } else {
             return res.status(400).json({
               password: 'Incorrect password'
