c9299e3b1c819a8ce9a37ea88cad9a8a622c7e44
Nick <nmatison@gmail.com>
2018-08-28 07:24:01 -0700

    add a entites reducer

--- public/javascripts/reducers/root_reducer.js
+++ public/javascripts/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducer from './auth_reducer';
 import SessionReducer from './session_reducer';
+import EntitiesReducer from './entities_reducer';
 
 export default combineReducers({
   tokens: AuthReducer,
-  session: SessionReducer
+  session: SessionReducer,
+  entities: EntitiesReducer
 });
--- public/javascripts/reducers/user_reducer.js
+++ public/javascripts/reducers/user_reducer.js
@@ -2,11 +2,13 @@ import merge from 'lodash/merge';
 import {RECEIVE_USER} from '../actions/user_actions';
 
 const UserReducer = (oldState = {}, action) => {
-  // Object.freeze(oldState);
-  // switch (action.type) {
-  //   case RECEIVE_USER:
-  //     return merge({}, oldState, {})
-  // }
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_USER:
+      return merge({}, oldState, {[action.user.id]: action.user});
+    default:
+      return oldState;
+  }
 }
 
 export default UserReducer;
