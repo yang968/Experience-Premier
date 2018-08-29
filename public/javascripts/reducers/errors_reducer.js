d1113794a208b3ea7f547cd63317f6095a93b361
Nick <nmatison@gmail.com>
2018-08-28 20:09:29 -0700

    Add action for task errors

--- public/javascripts/reducers/errors_reducer.js
+++ public/javascripts/reducers/errors_reducer.js
@@ -1,10 +1,13 @@
 import {RECEIVE_EMPLOYEE_ERRORS} from '../actions/employee_actions';
+import { RECEIVE_TASK_ERRORS } from '../actions/task_actions';
 
 const ErrorsReducer = (oldState = [], action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_EMPLOYEE_ERRORS:
       return action.errors 
+    case RECEIVE_TASK_ERRORS:
+      return action.errors
     default: 
       return oldState;
   }
