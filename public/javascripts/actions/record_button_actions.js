ae219c2f973ed8a099b80e78f5b477ec436901c2
Nick <nmatison@gmail.com>
2018-08-29 09:27:24 -0700

    add actions for the record button

---
+++ public/javascripts/actions/record_button_actions.js
@@ -0,0 +1,12 @@
+export const START_RECORDING = "START_RECORDING";
+export const STOP_RECORDING = "STOP_RECORDING";
+
+export const startRecording = button => ({
+  type: START_RECORDING:
+  button
+});
+
+export const stopRecording = button => ({
+  type: STOP_RECORDING,
+  button
+});
--- public/javascripts/components/tasks/task.jsx
+++ public/javascripts/components/tasks/task.jsx
@@ -15,39 +15,41 @@ class TaskPage extends React.Component {
     e.preventDefault();
     // fetching the token from our back-end
     this.props.fetchSpeechToken()
-      .then(response => {
-        return response.token
-      })
-      .then(token => {
-        // grab token and use Watson's node module to stream from the computer mic and send that sound file to 
-        // Watson's api for analysis.
-        var stream = recognizeMic({
-          token: token,
-          objectMode: true,
-          format: false,
-          word_confidence: true
-        });
-        // We aren't doing anything with our errors yet.
-        stream.on("error", err => {
-          console.log(err);
-        });
-        // If we receive a "data" response then we need to key into the data and display it by
-        // setting it to our state.
-        stream.on("data", data => {
-          //Watson sends multiple results per sentence, so we only want to display the "final"
-          //result. Otherwise we may have the same sentence displayed 1-3 times.
-          const final = data.results[0].final;
-          const text = data.results[0].alternatives[0].transcript;
-          if (final) {
-            let dataNode = document.createTextNode(text);
-            document.querySelector(".live-text").appendChild(dataNode);
-          }
-        });
-        // Vanilla DOM to select the start button and give it an onclick function to stop the stream.
-        document.querySelector(".record-button").onclick = stream.stop.bind(stream)
+    .then(response => {
+      return response.token
+    })
+    .then(token => {
+      // grab token and use Watson's node module to stream from the computer mic and send that sound file to 
+      // Watson's api for analysis.
+      var stream = recognizeMic({
+        token: token,
+        objectMode: true,
+        format: false,
+        word_confidence: true
       });
+      // We aren't doing anything with our errors yet.
+      stream.on("error", err => {
+        console.log(err);
+      });
+      // If we receive a "data" response then we need to key into the data and display it by
+      // setting it to our state.
+      stream.on("data", data => {
+        //Watson sends multiple results per sentence, so we only want to display the "final"
+        //result. Otherwise we may have the same sentence displayed 1-3 times.
+        const final = data.results[0].final;
+        const text = data.results[0].alternatives[0].transcript;
+        if (final) {
+          let dataNode = document.createTextNode(text);
+          document.querySelector(".live-text").appendChild(dataNode);
+        }
+      });
+      //select the start button and give it an onclick function to stop the stream.
+      //reset the start button to begin recording again.
+      // document.querySelector(".record-button").onclick = stream.stop.bind(stream)
+    });
   }
 
+  
 
   render() {
     return (
