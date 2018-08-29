c2467ea1e94ffa351d201cb388f6153edf30a51f
Jordan Yu <jordanyu44@gmail.com>
2018-08-29 13:07:06 -0700

    Begin styling dashboard components

--- public/javascripts/components/dashboard/dashboard_sidebar_container.js
+++ public/javascripts/components/dashboard/dashboard_sidebar_container.js
@@ -1,16 +1,16 @@
 import { connect } from 'react-redux';
 
 import DashboardSidebar from './dashboard_sidebar';
-import { logout } from '../../actions/'
+// import { logout } from '../../actions/';
 
 const mapStateToProps = ({session, entities: { users } }) => {
   return {
-    currentUser: users[session.id]
+    // currentUser: users[session.id]
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
+  // logout: () => dispatch(logout())
 });
 
 export default connect(
