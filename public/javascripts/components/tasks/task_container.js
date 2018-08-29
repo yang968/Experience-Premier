26a747e9815353640c817e7b700ba7865e7cc2f6
Nick <nmatison@gmail.com>
2018-08-28 20:39:27 -0700

    Add create task function to task page props

--- public/javascripts/components/tasks/task_container.js
+++ public/javascripts/components/tasks/task_container.js
@@ -1,13 +1,16 @@
-import {fetchSpeechToken} from '../../actions/auth_actions'
+import {fetchSpeechToken} from '../../actions/auth_actions';
+import {createTask} from '../../actions/task_actions';
 import {connect} from 'react-redux';
 import TaskPage from './task';
 
-const mapStateToProps = (state) => ({
-  blue: "blue"
-});
+const mapStateToProps = state => ({
+  token: state.session.currentUser.token
+})
+
 
 const mapDispatchToProps = dispatch => ({
-  fetchSpeechToken: () => dispatch(fetchSpeechToken())
+  fetchSpeechToken: () => dispatch(fetchSpeechToken()),
+  createTask: (task) => dispatch(createTask(task))
 });
 
 export default connect(
