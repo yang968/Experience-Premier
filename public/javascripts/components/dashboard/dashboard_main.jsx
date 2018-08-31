64d65d7bfff0f953d18a453ff891b0bf45d1e344
Jordan Yu <jordanyu44@gmail.com>
2018-08-30 20:10:52 -0700

    Set up performance and employees page structure

--- public/javascripts/components/dashboard/dashboard_main.jsx
+++ public/javascripts/components/dashboard/dashboard_main.jsx
@@ -6,16 +6,18 @@ import {
 
 import DashboardIndex from "./index/dashboard_index";
 import SpeechRecordContainer from "./speech_record/speech_record_container";
+import PerformancePage from './performance/performance_page';
+import EmployeesPage from './employees/employees_page';
 
 const DashboardMain = () => (
   <div className="dashboard-main-container">
     <Switch>
       <Route exact path="/dashboard" component={DashboardIndex} />
       <Route path="/dashboard/call" component={SpeechRecordContainer} />
-      <Route path="/dashboard/performance" component={() => <div></div>} />
-      <Route path="/dashboard/employees" component={() => <div></div>} />
+      <Route path="/dashboard/performance" component={PerformancePage} />
+      <Route path="/dashboard/employees" component={EmployeesPage} />
     </Switch>
   </div>
-)
+);
 
 export default DashboardMain;
