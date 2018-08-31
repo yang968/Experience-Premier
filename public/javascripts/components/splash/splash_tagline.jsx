7458abaf4f80db3553f92ae0bc3345154a50c412
Nick <nmatison@gmail.com>
2018-08-30 20:50:41 -0700

    Create demo login function for splash body/tagline container

--- public/javascripts/components/splash/splash_tagline.jsx
+++ public/javascripts/components/splash/splash_tagline.jsx
@@ -1,24 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Tagline = () => (
-  <div className="splash-tagline">
-    <section className="splash-tagline-box">
-      <h1>Take your customer service to the next level with ExP</h1>
-      <p>
-        ExP lets you analyze your own customer interactions and 
-        visualize your data so you know how to move forward 
-        and never stop improving. 
-      </p>
-      <Link to="/signup" className="session-button">
-        Demo ExP - It's free
-      </Link>
-      <span>
-        Already use ExP?&nbsp;
-        <Link to="/login">Log in.</Link>
-      </span>
-    </section>
-  </div>
-);
+class TagLine extends React.Component {
 
-export default Tagline;
+  constructor (props) {
+    super(props);
+    this.demoLogin = this.demoLogin.bind(this);
+  }
+
+  demoLogin() {
+    const demoUser = { email: "manager@manager.com", password: "12341234" };
+    this.props.login(demoUser).then(obj => {
+      window.localStorage.currentUser = JSON.stringify(obj.payload.currentUser);
+      window.localStorage.token = obj.payload.currentUser.token;
+    });
+  }
+
+  render() {
+    return (
+      <div className="splash-tagline">
+        <section className="splash-tagline-box">
+          <h1>Take your customer service to the next level with ExP</h1>
+          <p>
+            ExP lets you analyze your own customer interactions and 
+            visualize your data so you know how to move forward 
+            and never stop improving. 
+          </p>
+          <Link to="/dashboard" onClick={this.demoLogin} className="session-button">
+            Demo ExP - It's free
+          </Link>
+          <span>
+            Already use ExP?&nbsp;
+            <Link to="/login">Log in.</Link>
+          </span>
+        </section>
+      </div>
+    )
+  } 
+}
+
+
+export default withRouter(TagLine);
