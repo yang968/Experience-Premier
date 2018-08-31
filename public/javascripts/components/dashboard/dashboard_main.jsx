edfd4aeb3ee40070aa16220cd958f943ec3d01a8
Jordan Yu <jordanyu44@gmail.com>
2018-08-31 12:54:05 -0700

    Resolve merge conflicts

--- public/javascripts/components/dashboard/dashboard_main.jsx
+++ public/javascripts/components/dashboard/dashboard_main.jsx
@@ -6,13 +6,8 @@ import {
 
 import DashboardIndex from "./index/dashboard_index";
 import SpeechRecordContainer from "./speech_record/speech_record_container";
-<<<<<<< HEAD
-// import PerformancePage from './performance/performance_page';
-// import EmployeesPage from './employees/employees_page';
-=======
 import PerformancePage from './performance/performance_page';
 import EmployeesPageContainer from './employees/employees_page_container';
->>>>>>> d818db100be770a6af7194cbce7262a203c276d6
 
 import { AuthRoute, ProtectedRoute } from "../../util/route_util";
 
@@ -21,13 +16,8 @@ const DashboardMain = () => (
     <Switch>
       <ProtectedRoute exact path="/dashboard" component={DashboardIndex} />
       <ProtectedRoute path="/dashboard/call" component={SpeechRecordContainer} />
-<<<<<<< HEAD
-      {/* <ProtectedRoute path="/dashboard/performance" component={() => <div></div>} /> */}
-      {/* <ProtectedRoute path="/dashboard/employees" component={() => <div></div>} /> */}
-=======
       <ProtectedRoute path="/dashboard/performance" component={PerformancePage} />
       <ProtectedRoute path="/dashboard/employees" component={EmployeesPageContainer} />
->>>>>>> d818db100be770a6af7194cbce7262a203c276d6
     </Switch>
   </div>
 );
