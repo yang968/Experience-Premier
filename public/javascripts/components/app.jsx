81982472ca70a0c206681510a0a96c9d2543ed81
Jordan Yu <jordanyu44@gmail.com>
2018-08-30 19:46:38 -0700

    Make only last splash header button green

--- public/javascripts/components/app.jsx
+++ public/javascripts/components/app.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
+import { AuthRoute, ProtectedRoute } from "../util/route_util"
+
 import SplashHeaderContainer from './splash/splash_header_container';
 import SplashBody from './splash/splash_body';
 import FooterBody from './global_footer/footer_body';
-import DashboardPage from './dashboard/dashboard_page';
-import DashboardSidebarContainer from './dashboard/sidebar/dashboard_sidebar_container';
-import DashboardMain from './dashboard/dashboard_main';
-import { AuthRoute, ProtectedRoute } from "../util/route_util"
 import LoginFormContainer from './splash/login_form_container';
-
+import ContactPage from './info/contact_page';
+import DashboardPage from './dashboard/dashboard_page';
 
 import { Route,
          Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
-      <AuthRoute exact path="/" component={SplashHeaderContainer} />
+      <AuthRoute path="/" component={SplashHeaderContainer} />
       <AuthRoute exact path="/" component={SplashBody} />
       <AuthRoute exact path="/" component={FooterBody} />
       <AuthRoute exact path="/" component={LoginFormContainer} />
+      <Route path="/contact" component={ContactPage} />
       <ProtectedRoute path="/dashboard" component={DashboardPage} />
   </div>
 )
---
+++ public/javascripts/components/info/contact_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactPage = () => (
+  <div className="contact-page-container">
+    Here be contacts.
+  </div>
+)
+
+export default ContactPage;
