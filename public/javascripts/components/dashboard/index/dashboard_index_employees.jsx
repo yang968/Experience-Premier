0612b3ae8d8bbc97d821533e24c7e5b1d8f52b8e
Nick <nmatison@gmail.com>
2018-08-31 09:48:21 -0700

    fix issue with props mapping for employees

--- public/javascripts/components/dashboard/index/dashboard_index_employees.jsx
+++ public/javascripts/components/dashboard/index/dashboard_index_employees.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const DashboardIndexEmployees = () => (
-  <div className="dashboard-index-employees-container">
-    EMPLOYEES
-  </div>
-);
+class DashboardIndexEmployees extends React.Component {
+
+  construtor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+    <div className="dashboard-index-employees-container">
+      EMPLOYEES
+    </div>
+    );
+  }
+};
 
 export default DashboardIndexEmployees;
