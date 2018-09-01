1da1af2ec466a1fff04024eb5dcc2176a4351682
Nick <nmatison@gmail.com>
2018-08-31 19:09:28 -0700

    fix employee login information

--- public/javascripts/components/dashboard/dashboard_main.jsx
+++ public/javascripts/components/dashboard/dashboard_main.jsx
@@ -4,30 +4,20 @@ import {
   Switch
 } from 'react-router-dom';
 
-import DashboardIndex from "./index/dashboard_index";
+import DashboardIndexContainer from "./index/dashboard_index_container";
 import SpeechRecordContainer from "./speech_record/speech_record_container";
-<<<<<<< HEAD
 // import PerformancePage from './performance/performance_page';
 // import EmployeesPage from './employees/employees_page';
-=======
-import PerformancePage from './performance/performance_page';
-import EmployeesPageContainer from './employees/employees_page_container';
->>>>>>> d818db100be770a6af7194cbce7262a203c276d6
 
 import { AuthRoute, ProtectedRoute } from "../../util/route_util";
 
 const DashboardMain = () => (
   <div className="dashboard-main-container">
     <Switch>
-      <ProtectedRoute exact path="/dashboard" component={DashboardIndex} />
+      <ProtectedRoute exact path="/dashboard" component={DashboardIndexContainer} />
       <ProtectedRoute path="/dashboard/call" component={SpeechRecordContainer} />
-<<<<<<< HEAD
       {/* <ProtectedRoute path="/dashboard/performance" component={() => <div></div>} /> */}
       {/* <ProtectedRoute path="/dashboard/employees" component={() => <div></div>} /> */}
-=======
-      <ProtectedRoute path="/dashboard/performance" component={PerformancePage} />
-      <ProtectedRoute path="/dashboard/employees" component={EmployeesPageContainer} />
->>>>>>> d818db100be770a6af7194cbce7262a203c276d6
     </Switch>
   </div>
 );
--- public/javascripts/components/dashboard/index/dashboard_index.jsx
+++ public/javascripts/components/dashboard/index/dashboard_index.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import DashboardIndexStatsContainer from './dashboard_index_stats_container';
-import DashboardIndexEmployeesContainer from './dashboard_index_employees_container';
-import DashboardIndexCallHistoryContainer from './dashboard_index_call_history_container';
+import DashboardIndexStatsContainer from "./dashboard_index_stats_container";
+import DashboardIndexEmployeesContainer from "./dashboard_index_employees_container";
+import DashboardIndexCallHistoryContainer from "./dashboard_index_call_history_container";
 
-const DashboardIndex = () => (
+const renderEmployees = (employees) => {
+  if (employees.length > 0) return <DashboardIndexEmployeesContainer />;
+  return null;
+};
+
+const DashboardIndex = ({employees}) => (
   <div className="dashboard-index-container">
     <DashboardIndexStatsContainer />
-    <DashboardIndexEmployeesContainer />
+    {renderEmployees(employees)}
     <DashboardIndexCallHistoryContainer />
   </div>
 );
