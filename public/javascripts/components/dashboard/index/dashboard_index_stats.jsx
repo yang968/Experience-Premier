b876b7435253b263bce11324a1063c9f70dc07e2
Nick <nmatison@gmail.com>
2018-08-31 19:22:51 -0700

    fix component will mount issue

--- public/javascripts/components/dashboard/index/dashboard_index_stats.jsx
+++ public/javascripts/components/dashboard/index/dashboard_index_stats.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Pie, Bar } from 'react-chartjs-2';
 import { COLOR, POSITIVITY_LABELS, SENTIMENT_LABELS } from "../../../chart/chart";
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 class DashboardIndexStats extends React.Component {
 	constructor(props) {
@@ -23,13 +24,15 @@ class DashboardIndexStats extends React.Component {
   }
 
   componentWillMount() {
-    let count = this.props.stats[0].tasks;
-    let stats = this.props.stats[0];
-    let [neg, neu, pos] = [stats.negative, stats.neutral, stats.positive];
-    let [s, j, a, f, d] = [stats.sadness, stats.joy, stats.anger, stats.fear, stats.disgust];
-    let data = [s, j, a, f, d].map((el) => (el/count).toFixed(3));
-    this.state.chartData1.datasets[0].data = [neg, neu, pos];
-    this.state.chartData2.datasets[0].data = data;
+    if (this.props.stats.length > 0) {
+      let count = this.props.stats[0].tasks;
+      let stats = this.props.stats[0];
+      let [neg, neu, pos] = [stats.negative, stats.neutral, stats.positive];
+      let [s, j, a, f, d] = [stats.sadness, stats.joy, stats.anger, stats.fear, stats.disgust];
+      let data = [s, j, a, f, d].map((el) => (el/count).toFixed(3));
+      this.state.chartData1.datasets[0].data = [neg, neu, pos];
+      this.state.chartData2.datasets[0].data = data;
+    }
   }
 
 	render() {
