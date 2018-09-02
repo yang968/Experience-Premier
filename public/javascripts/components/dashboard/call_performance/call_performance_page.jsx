d61c8930c3c4fdc912404757806c69ecf2e75cf9
mark <m.arklee@outlook.com>
2018-09-01 17:30:09 -0700

    Remove grid and axes for performance result chart

--- public/javascripts/components/dashboard/call_performance/call_performance_page.jsx
+++ public/javascripts/components/dashboard/call_performance/call_performance_page.jsx
@@ -22,15 +22,28 @@ class CallPerformancePage extends React.Component {
 			},
 			options : { 
 				legend: { display: false },
-				scales: { xAxes: [{ stacked: true }], yAxes: [{ stacked: true }]}
+				scales: { 
+					xAxes: [{ 
+						stacked: true,
+						display: false, 
+						gridLines: {display: false} }],
+					yAxes: [{ 
+						stacked: true,
+						display: false, 
+						gridLines: {display: false}
+					}]},
 			},
 			datasets: [{ 
 				label: "Sadness",
-				backgroundColor: 'yellow',
+				backgroundColor: 'rgba(40, 122, 255, 0.72)',
 				data: [1] 
 			},{ 
 				label: "Joy",
-				backgroundColor: 'red',
+				backgroundColor: 'rgba(245, 250, 30, 0.72)',
+				data: [5] 
+			}, { 
+				label: "Fear",
+				backgroundColor: 'rgba(255, 40, 100, 0.72)',
 				data: [2] 
 			}]
 		};
@@ -39,6 +52,9 @@ class CallPerformancePage extends React.Component {
 	componentWillMount() {
 		this.state.score = this.props.stats.results.sentiment.score;
 		this.state.label = this.props.stats.results.sentiment.label;
+		console.log(this.props.stats.results.emotion);
+		let emo = this.props.stats.results.emotion;
+		let emotions = [emo.sadness, emo.joy, emo.anger, emo.fear, emo.disgust];
 	}
 
 	render() {
@@ -47,7 +63,9 @@ class CallPerformancePage extends React.Component {
 						<p>Label: {this.state.label}</p>
 						<p>Score: {this.state.score}</p>
 					</div>
-					<HorizontalBar data={{ labels: ['Sentiment Analysis'], datasets: this.state.datasets}} 
+					<HorizontalBar data={{ 
+							labels: ['Sentiment Analysis'], 
+							datasets: this.state.datasets }} 
 						options={this.state.options} 
 					/>
 				</div>;
