2dee482e29d6fe67f8ae62837554b6e21b1ea1fe
Nick <nmatison@gmail.com>
2018-09-02 16:16:20 -0700

    Remove actions from window

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import Favicon from 'react-favicon';
 import Root from './components/root';
 import configureStore from "./store/store";
-import {createUser, login, logout} from './actions/employee_actions';
-import {createTask, fetchTask, deleteTask} from './actions/task_actions';
 
 import "../stylesheets/index.scss";
 
@@ -20,14 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-  window.login = login;
-  window.logout = logout;
-  window.createUser = createUser;
-  window.createTask = createTask;
-  window.fetchTask = fetchTask;
-  window.deleteTask = deleteTask;
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
+  
   ReactDOM.render(<div>
       <Favicon url="https://i.imgur.com/QhiuZTl.png" />
       <Root store={store} />
