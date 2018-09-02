d9d51c524945b0f4de0fa738d88fea2dfdcdaf25
Nick <nmatison@gmail.com>
2018-09-02 13:43:48 -0700

    Space out logos on contact page

--- public/javascripts/components/info/contact_page.jsx
+++ public/javascripts/components/info/contact_page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const ContactPage = () => (
   <div className="contact-page-container">
@@ -18,16 +18,22 @@ const ContactPage = () => (
               href="https://github.com/yang968"
               target="_blank"
             >
-              <img className="github-image" src={require("../../../images/github-logo.png")} />
+              <img
+                className="github-image"
+                src={require("../../../images/github-logo.png")}
+              />
             </a>
             <a
               className="linked-in"
               href="https://www.linkedin.com/in/yang53/"
               target="_blank"
             >
-              <img className="linkedin-image" src={require("../../../images/linkedin-logo.png")} />
+              <img
+                className="linkedin-image"
+                src={require("../../../images/linkedin-logo.png")}
+              />
             </a>
-            </div>
+          </div>
         </section>
       </li>
       <li className="team-member">
@@ -45,14 +51,20 @@ const ContactPage = () => (
               href="https://github.com/nmatison"
               target="_blank"
             >
-              <img className="github-image" src={require("../../../images/github-logo.png")} />
+              <img
+                className="github-image"
+                src={require("../../../images/github-logo.png")}
+              />
             </a>
             <a
               className="linked-in"
               href="https://www.linkedin.com/in/nicholas-matison-066359108/"
               target="_blank"
             >
-              <img className="linkedin-image" src={require("../../../images/linkedin-logo.png")} />
+              <img
+                className="linkedin-image"
+                src={require("../../../images/linkedin-logo.png")}
+              />
             </a>
           </div>
         </section>
@@ -72,14 +84,20 @@ const ContactPage = () => (
               href="https://github.com/marklee9"
               target="_blank"
             >
-              <img className="github-image" src={require("../../../images/github-logo.png")} />
+              <img
+                className="github-image"
+                src={require("../../../images/github-logo.png")}
+              />
             </a>
             <a
               className="linked-in"
               href="https://www.linkedin.com/in/m-arklee/"
               target="_blank"
             >
-              <img className="linkedin-image" src={require("../../../images/linkedin-logo.png")} />
+              <img
+                className="linkedin-image"
+                src={require("../../../images/linkedin-logo.png")}
+              />
             </a>
           </div>
         </section>
@@ -99,14 +117,20 @@ const ContactPage = () => (
               href="https://github.com/JordanYu4"
               target="_blank"
             >
-              <img className="github-image" src={require("../../../images/github-logo.png")} />
+              <img
+                className="github-image"
+                src={require("../../../images/github-logo.png")}
+              />
             </a>
             <a
               className="linked-in"
               href="https://www.linkedin.com/in/jordan-yu-a7119b80/"
               target="_blank"
             >
-              <img className="linkedin-image" src={require("../../../images/linkedin-logo.png")} />
+              <img
+                className="linkedin-image"
+                src={require("../../../images/linkedin-logo.png")}
+              />
             </a>
           </div>
         </section>
