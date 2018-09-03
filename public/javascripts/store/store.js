883e387cf9ecf15a732ee5a8c2c0f3fb6800e52e
yang968 <spenceryang89@gmail.com>
2018-09-02 21:12:36 -0700

    Add logger in store.js

--- public/javascripts/store/store.js
+++ public/javascripts/store/store.js
@@ -7,7 +7,7 @@ const configureStore = (preLoadedState = {}) => {
   return createStore(
     rootReducer,
     preLoadedState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   );
 }
 
