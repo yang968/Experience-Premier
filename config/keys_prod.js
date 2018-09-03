cd3c1d5e0aea569fe2aa15f3d72d34013d1b7296
Jordan Yu <jordanyu44@gmail.com>
2018-09-02 18:54:01 -0700

    Set up dev and prod keys

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  emotionUsername: process.env.EMOTION_USERNAME,
+  emotionPassword: process.env.EMOTION_PASSWORD
+};
+
