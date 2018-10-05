d56192404cd09975761ebda122f769def5393739
Nick <nmatison@gmail.com>
2018-10-04 21:52:02 -0700

    Clean up unneeded comments

--- public/javascripts/util/user_api_util.js
+++ public/javascripts/util/user_api_util.js
@@ -1,35 +1,26 @@
 import axios from "axios";
 import formurlencoded from 'form-urlencoded/dist/form-urlencoded';
 
-// if (process.env.No)
-
 export const createUser = (user) => {
   let instance = axios.create({});
   instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
   return instance.post("/api/users/register", formurlencoded(user))
 }
 
-// export const login = (user) => {
-//   let instance = axios.create({});
-//   instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'; 
-//   return instance.post("/api/users/login", formurlencoded(user))
-// };
-
 export const login = (user) => {
   return (
   fetch("/api/users/login", {
     method: "POST",
     mode: "cors",
-    credentials: "same-origin", // include, same-origin, *omit
+    credentials: "same-origin", 
     headers: {
         "Content-Type": "application/x-www-form-urlencoded",
     },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: formurlencoded(user), // body data type must match "Content-Type" header
+    redirect: "follow",
+    referrer: "no-referrer",
+    body: formurlencoded(user),
     }).then((res) => res.json()).then((payload) => {return payload})
-    )
-  };
+  )};
 
 
 export const logout = (token) => {
