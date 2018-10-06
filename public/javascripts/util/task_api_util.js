211a7bdd2c4ea155ed6cce44e8d299e849a8d668
Nick <nmatison@gmail.com>
2018-10-06 09:07:17 -0700

    Convert the rest of task util to fetch from axios

--- public/javascripts/util/task_api_util.js
+++ public/javascripts/util/task_api_util.js
@@ -1,26 +1,35 @@
 import axios from 'axios';
 import formurlencoded from 'form-urlencoded/dist/form-urlencoded';
 
-export const createTask = (taskData) => {
-  let authToken = taskData.token
-  let instance = axios.create({});
-  instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-  instance.defaults.headers.common[`Authorization`] = authToken;
-  return instance.post("/api/tasks/", formurlencoded(taskData));
-}
-
-// export const fetchTask = (taskData) => {
-//   let authToken = taskData.token;
+// export const createTask = (taskData) => {
+//   let authToken = taskData.token
 //   let instance = axios.create({});
 //   instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 //   instance.defaults.headers.common[`Authorization`] = authToken;
-//   return instance.get(`/api/tasks/${taskData._id}`);
+//   return instance.post("/api/tasks/", formurlencoded(taskData));
 // }
+
+export const createTask = (taskData) => {
+  let authToken = taskData.token;
+  return (
+    fetch("/api/tasks/", {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": authToken
+      },
+      redirect: "follow",
+      referrer: "no-referrer"
+    }).then((res) => res.json()).then((response) => {return response})
+  );
+};
 
 export const fetchTask = (taskData) => {
   let authToken = taskData.token;
   return (
-    fetch(`/api/users/${taskData._id}`, {
+    fetch(`/api/tasks/${taskData._id}`, {
       method: "GET",
       mode: "cors",
       credentials: "same-origin",
@@ -34,10 +43,27 @@ export const fetchTask = (taskData) => {
   );
 }
 
+// export const deleteTask = (taskData) => {
+//   let authToken = taskData.token;
+//   let instance = axios.create({});
+//   instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+//   instance.defaults.headers.common[`Authorization`] = authToken;
+//   return instance.delete(`/api/tasks/${taskData._id}`);
+// }
+
 export const deleteTask = (taskData) => {
   let authToken = taskData.token;
-  let instance = axios.create({});
-  instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-  instance.defaults.headers.common[`Authorization`] = authToken;
-  return instance.delete(`/api/tasks/${taskData._id}`);
+  return (
+    fetch(`/api/tasks/${taskData._id}`, {
+      method: "DELETE",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": authToken
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
+    }).then((res) => res.json()).then((response) => { return response })
+  );
 }
