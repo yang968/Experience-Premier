87e4db2db56f9c2dea2e51d657cc894c796d8a18
Nick <nmatison@gmail.com>
2018-10-06 08:58:59 -0700

    fetch task converted to fetch from axios

--- public/javascripts/util/task_api_util.js
+++ public/javascripts/util/task_api_util.js
@@ -9,12 +9,29 @@ export const createTask = (taskData) => {
   return instance.post("/api/tasks/", formurlencoded(taskData));
 }
 
+// export const fetchTask = (taskData) => {
+//   let authToken = taskData.token;
+//   let instance = axios.create({});
+//   instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+//   instance.defaults.headers.common[`Authorization`] = authToken;
+//   return instance.get(`/api/tasks/${taskData._id}`);
+// }
+
 export const fetchTask = (taskData) => {
   let authToken = taskData.token;
-  let instance = axios.create({});
-  instance.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-  instance.defaults.headers.common[`Authorization`] = authToken;
-  return instance.get(`/api/tasks/${taskData._id}`);
+  return (
+    fetch(`/api/users/${taskData._id}`, {
+      method: "GET",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": authToken
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
+    }).then((res) => res.json()).then((response) => { return response })
+  );
 }
 
 export const deleteTask = (taskData) => {
