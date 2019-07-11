86f8e24168705c2be9d2fdc58ff6a1c60e309486
Jordan Yu <jordan.yu@punchh.com>
2019-07-11 16:43:45 -0700

    Tighten up HTML

--- public/javascripts/components/info/contact_page.jsx
+++ public/javascripts/components/info/contact_page.jsx
@@ -4,128 +4,108 @@ const ContactPage = () => (
   <div className="contact-page-container">
     <ul className="team-list animated slideInLeft">
       <li className="team-member">
-        <img
-          src={require("../../../images/victor-pro-pic.jpg")}
+        <img src={require("../../../images/victor-pro-pic.jpg")}
           alt="Victor Yang"
           className="profile-pic"
         />
         <section>
           <h3>Victor Yang</h3>
           <div className="logo-div animated fadeIn">
-            <a
+            <a href="https://github.com/yang968"
               className="github"
-              href="https://github.com/yang968"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/github-logo.png")}
                 className="github-image"
-                src={require("../../../images/github-logo.png")}
               />
             </a>
-            <a
+            <a href="https://www.linkedin.com/in/yang53/"
               className="linked-in"
-              href="https://www.linkedin.com/in/yang53/"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/linkedin-logo.png")}
                 className="linkedin-image"
-                src={require("../../../images/linkedin-logo.png")}
               />
             </a>
           </div>
         </section>
       </li>
       <li className="team-member">
-        <img
-          src={require("../../../images/nick-pro-pic.jpg")}
+        <img src={require("../../../images/nick-pro-pic.jpg")}
           alt="Nick Matison"
           className="profile-pic"
         />
         <section>
           <h3>Nick Matison</h3>
           <div className="logo-div animated fadeIn">
-            <a
+            <a href="https://github.com/nmatison"
               className="github"
-              href="https://github.com/nmatison"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/github-logo.png")}
                 className="github-image"
-                src={require("../../../images/github-logo.png")}
               />
             </a>
-            <a
+            <a href="https://www.linkedin.com/in/nicholas-matison-066359108/"
               className="linked-in"
-              href="https://www.linkedin.com/in/nicholas-matison-066359108/"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/linkedin-logo.png")}
                 className="linkedin-image"
-                src={require("../../../images/linkedin-logo.png")}
               />
             </a>
           </div>
         </section>
       </li>
       <li className="team-member">
-        <img
-          src={require("../../../images/mark-pro-pic.jpg")}
+        <img src={require("../../../images/mark-pro-pic.jpg")}
           alt="Mark Lee"
           className="profile-pic"
         />
         <section>
           <h3>Mark Lee</h3>
           <div className="logo-div animated fadeIn">
-            <a
+            <a href="https://github.com/marklee9"
               className="github"
-              href="https://github.com/marklee9"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/github-logo.png")}
                 className="github-image"
-                src={require("../../../images/github-logo.png")}
               />
             </a>
-            <a
+            <a href="https://www.linkedin.com/in/m-arklee/"
               className="linked-in"
-              href="https://www.linkedin.com/in/m-arklee/"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/linkedin-logo.png")}
                 className="linkedin-image"
-                src={require("../../../images/linkedin-logo.png")}
               />
             </a>
           </div>
         </section>
       </li>
       <li className="team-member">
-        <img
-          src={require("../../../images/jordan-pro-pic.jpg")}
+        <img src={require("../../../images/jordan-pro-pic.jpg")}
           alt="Jordan Yu"
           className="profile-pic"
         />
         <section>
           <h3>Jordan Yu</h3>
           <div className="logo-div animated fadeIn">
-            <a
+            <a href="https://github.com/JordanYu4"
               className="github"
-              href="https://github.com/JordanYu4"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/github-logo.png")}
                 className="github-image"
-                src={require("../../../images/github-logo.png")}
               />
             </a>
-            <a
+            <a href="https://www.linkedin.com/in/jordan-yu-4400/"
               className="linked-in"
-              href="https://www.linkedin.com/in/jordan-yu-a7119b80/"
               target="_blank"
             >
-              <img
+              <img src={require("../../../images/linkedin-logo.png")}
                 className="linkedin-image"
-                src={require("../../../images/linkedin-logo.png")}
               />
             </a>
           </div>
